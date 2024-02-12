147358a45ea7f089780792c23b7fc6793c229c3c
Ryan Drachenberg <tssinvestments@gmail.com>
2024-02-12 17:57:52 -0500

    cleaned up projects page

--- data/projects.ts
+++ data/projects.ts
@@ -54,7 +54,7 @@ export const projects = [
   {
     name: "Dashey",
     description:
-      "A Nextjs styled dashboard with Railway Postgres db, Prisma(ORM), Tremor(charts & graphs), Next Auth",
+      "A Next Auth app with Railway Postgres db, Prisma(ORM), Tremor(charts & graphs)",
     link: { 
       href: "https://dashey.vercel.app/", 
       label: "dashey.vercel.app" 
