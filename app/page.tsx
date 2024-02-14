557925bd276e70a93b04a73f35de64839ec697cd
Ryan Drachenberg <tssinvestments@gmail.com>
2024-02-13 20:16:09 -0500

    changed some text

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function HomePage() {
     <>
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Writing on software design, company building, and the blockchain industry.
+          Writing on software design, digital assets, and the blockchain industry.
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-        All of my long-form thoughts on programming, leadership, product design, and
+        My thoughts on programming, e-commerce, product design, and
         more, collected in chronological order.
         </p>
       </header>
