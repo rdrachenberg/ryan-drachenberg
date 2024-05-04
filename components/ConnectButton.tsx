d2e7847feacf302011a3c7fd871625d1b566eb29
Ryan Drachenberg <tssinvestments@gmail.com>
2024-05-04 16:53:14 -0400

    defaulted button text to white regardless of dark/light theme swaps

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -9,7 +9,7 @@ export default function ConnectButton(){
     return (
         <div>
         {isConnected ? <div className=''><w3m-button balance='show' /></div> : <div>
-                <button onClick={() => open()} className='rounded-full p-2 w-[150px] bg-blue-500 border-blue-500'>Connect Wallet</button>
+                <button onClick={() => open()} className='rounded-full p-2 w-[150px] bg-blue-500 border-blue-500 text-white'>Connect Wallet</button>
             </div>}
         </div>
     )
